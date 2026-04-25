3dbe262bb2456962fe518017ebece5b1fc85282e
Julio Celestino <velocirraptorrex3@gmail.com>
2026-04-25 11:46:05 -0500

    Update 1 Add player

---
+++ Taller3DExamen1/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taller3DExamen1
+{
+    internal class Player
+    {
+        private string Name;
+        private int Life;
+        private int Damage;
+
+        public Player(string name)
+        {
+            Name = name;
+            Life = 100;
+            Damage = 10;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public int GetLife()
+        {
+            return Life;
+        }
+
+        public int GetDamage()
+        {
+            return Damage;
+        }
+    }
+}
--- Taller3DExamen1/Program.cs
+++ Taller3DExamen1/Program.cs
@@ -1,1 +1,41 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace Taller3DExamen1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string optionMain="";
+            while (optionMain != "0")
+            {
+                Console.WriteLine("BUSCANDO TU MANADA");
+                Console.WriteLine("1. Jugar");
+                Console.WriteLine("2. Salir");
+                optionMain = Console.ReadLine();   
+                switch (optionMain)
+                {
+                    case "1":
+                        Play();
+                        break;
+                    case "2":
+                        Console.WriteLine("Salir");
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida. Por favor, elige una opción válida.");
+                        break;
+                }
+            }
+        }
+        private static void Play()
+        {
+            Console.Clear();
+            Console.WriteLine("Escribe el nombre de tu personaje:");
+            string name = Console.ReadLine();
+
+            Player player = new Player(name);
+
+            Console.WriteLine("Nombre: " + player.GetName());
+            Console.WriteLine("Vida: " + player.GetLife());
+            Console.WriteLine("Daño: " + player.GetDamage());
+        }
+    }
+}
